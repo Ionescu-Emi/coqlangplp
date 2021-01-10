824a76d84549b5d2f5f757925ba312f6cb87a315
Ionescu-Emi <33662821+Ionescu-Emi@users.noreply.github.com>
2021-01-10 22:24:20 +0200

    Update Sintaxa.v

--- Sintaxa.v
+++ Sintaxa.v
@@ -143,7 +143,7 @@ Inductive Stmt :=
 | vec_bools : Var -> nat -> Stmt
 | vec_access : Var -> nat -> Stmt
 | function_call : Var -> list Val -> Stmt
-| function_definition : Var -> nat -> Stmt -> Stmt
+| function_definition : Var -> list Val -> Stmt -> Stmt
 | class' : Var -> Stmt -> Stmt
 | object_declaration : Var -> Var -> Stmt
 | class_var_access : Var -> Var -> Stmt
